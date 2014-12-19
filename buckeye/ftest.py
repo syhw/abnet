2639f289bff58a2a7fc9743168888e423a713017
syhw <gabriel.synnaeve@gmail.com>
2014-12-19 14:14:50 +0100

    dirty F-test scripts for ICLR workshop paper

---
+++ buckeye/ftest.py
@@ -0,0 +1,92 @@
+import cPickle, sys, glob, os
+from collections import defaultdict
+import numpy as np
+
+NFRAMES_PER_SEC = 100
+#bdir = "deep_coscos2_buckeye_dtw_word_spkr_fbank11_ab_net_adadelta_emb_100/"
+bdir = sys.argv[1].rstrip('/') + '/'
+phndir = "buckeye_modified_split_devtest/phn/*/"
+
+
+def parse(fn):
+    try:
+        with open(fn) as rf:
+            li = map(lambda x: x.rstrip('\n').split(), rf.readlines())
+            li = map(lambda x: (float(x[0]), float(x[1]), x[2]), li)
+            #print fn, "found"
+            return li
+    except IOError:
+        print fn, "not found => skipping it"
+        return []
+
+
+for i in range(3):
+    dir = bdir + "layer_" + str(i) + '/'
+
+    spkrs = defaultdict(lambda: [np.zeros(500), np.zeros(500), 0])  # {'sid':[500 (mean, mean**2, length)]}
+    phns = defaultdict(lambda: [np.zeros(500), np.zeros(500), 0])
+    all = [np.zeros(500), np.zeros(500), 0] # [500 (mean, mean**2, length)]
+
+    for fname in glob.iglob(dir + "*.npz"):
+        npz = np.load(fname)
+        npz_fn = fname.split('/')[-1]
+        phn_fn = [x for x in glob.iglob(phndir + npz_fn.split('.')[0] + '.phn')][0]
+        phones = parse(phn_fn)
+        spkr = npz_fn[:3]
+        t = npz['features']
+        t2 = t**2
+        s = np.sum(t, axis=0) # (length, 500)
+        s2 = np.sum(t2, axis=0)
+        l = t.shape[0]
+        spkrs[spkr][0] += s
+        spkrs[spkr][1] += s2
+        spkrs[spkr][2] += l
+        for start, end, phn in phones:
+            tt = t[start*NFRAMES_PER_SEC:end*NFRAMES_PER_SEC]
+            tt2 = t2[start*NFRAMES_PER_SEC:end*NFRAMES_PER_SEC]
+            phns[phn][0] += np.sum(tt, axis=0)
+            phns[phn][1] += np.sum(tt2, axis=0)
+            phns[phn][2] += tt.shape[0]
+        all[0] += s
+        all[1] += s2
+        all[2] += l
+    N = all[2]
+    means = all[0] / N
+    # SPEAKERS
+    print spkrs.keys()
+    K = len(spkrs)
+    assert(K > 1)
+    means_spkrs = {}
+    vars_spkrs = {}
+    for spkr, (s, s2, l) in spkrs.iteritems():
+        m = s/l
+        means_spkrs[spkr] = m
+        vars_spkrs[spkr] = s2/l - m**2
+    between_vars = np.zeros(500)
+    for spkr, m in means_spkrs.iteritems():
+        between_vars += spkrs[spkr][2] * (m - means)**2 / (K - 1)
+    within_vars = np.zeros(500)
+    for v in vars_spkrs.itervalues():
+        within_vars += v / (N - K)
+    F = between_vars / within_vars
+    with open(bdir + 'F_spkrs_layer_' + str(i) + '.npy', 'wb') as f:
+        F.dump(f)
+    # PHONES
+    print phns.keys()
+    K = len(phns)
+    assert(K > 1)
+    means_phns = {}
+    vars_phns = {}
+    for phn, (s, s2, l) in phns.iteritems():
+        m = s/l
+        means_phns[phn] = m
+        vars_phns[phn] = s2/l - m**2
+    between_vars = np.zeros(500)
+    for phn, m in means_phns.iteritems():
+        between_vars += phns[phn][2] * (m - means)**2 / (K - 1)
+    within_vars = np.zeros(500)
+    for v in vars_phns.itervalues():
+        within_vars += v / (N - K)
+    F = between_vars / within_vars
+    with open(bdir + 'F_phns_layer_' + str(i) + '.npy', 'wb') as f:
+        F.dump(f)
---
+++ buckeye/ftest_embs.py
@@ -0,0 +1,91 @@
+import cPickle, sys, glob, os
+from collections import defaultdict
+import numpy as np
+
+NFRAMES_PER_SEC = 100
+bdir = sys.argv[1].rstrip('/') + '/'
+phndir = "buckeye_modified_split_devtest/phn/*/"
+
+
+def parse(fn):
+    try:
+        with open(fn) as rf:
+            li = map(lambda x: x.rstrip('\n').split(), rf.readlines())
+            li = map(lambda x: (float(x[0]), float(x[1]), x[2]), li)
+            #print fn, "found"
+            return li
+    except IOError:
+        print fn, "not found => skipping it"
+        return []
+
+
+dir = bdir
+
+spkrs = defaultdict(lambda: [np.zeros(100), np.zeros(100), 0])  # {'sid':[100 (mean, mean**2, length)]}
+phns = defaultdict(lambda: [np.zeros(100), np.zeros(100), 0])
+all = [np.zeros(100), np.zeros(100), 0] # [100 (mean, mean**2, length)]
+
+for fname in glob.iglob(dir + "*.npz"):
+    npz = np.load(fname)
+    npz_fn = fname.split('/')[-1]
+    phn_fn = [x for x in glob.iglob(phndir + npz_fn.split('.')[0] + '.phn')][0]
+    phones = parse(phn_fn)
+    spkr = npz_fn[:3]
+    t = npz['features']
+    t2 = t**2
+    s = np.sum(t, axis=0) # (length, 100)
+    s2 = np.sum(t2, axis=0)
+    l = t.shape[0]
+    spkrs[spkr][0] += s
+    spkrs[spkr][1] += s2
+    spkrs[spkr][2] += l
+    for start, end, phn in phones:
+        tt = t[start*NFRAMES_PER_SEC:end*NFRAMES_PER_SEC]
+        tt2 = t2[start*NFRAMES_PER_SEC:end*NFRAMES_PER_SEC]
+        phns[phn][0] += np.sum(tt, axis=0)
+        phns[phn][1] += np.sum(tt2, axis=0)
+        phns[phn][2] += tt.shape[0]
+    all[0] += s
+    all[1] += s2
+    all[2] += l
+N = all[2]
+means = all[0] / N
+# SPEAKERS
+print spkrs.keys()
+K = len(spkrs)
+assert(K > 1)
+means_spkrs = {}
+vars_spkrs = {}
+for spkr, (s, s2, l) in spkrs.iteritems():
+    m = s/l
+    means_spkrs[spkr] = m
+    vars_spkrs[spkr] = s2/l - m**2
+between_vars = np.zeros(100)
+for spkr, m in means_spkrs.iteritems():
+    between_vars += spkrs[spkr][2] * (m - means)**2 / (K - 1)
+within_vars = np.zeros(100)
+for v in vars_spkrs.itervalues():
+    within_vars += v / (N - K)
+F = between_vars / within_vars
+suffix = bdir.rstrip('/').split('.')[0].split('_')[-1]
+with open('F_spkrs_emb_' + suffix + '.npy', 'wb') as f:
+    F.dump(f)
+# PHONES
+print phns.keys()
+K = len(phns)
+assert(K > 1)
+means_phns = {}
+vars_phns = {}
+for phn, (s, s2, l) in phns.iteritems():
+    m = s/l
+    means_phns[phn] = m
+    vars_phns[phn] = s2/l - m**2
+between_vars = np.zeros(100)
+for phn, m in means_phns.iteritems():
+    between_vars += phns[phn][2] * (m - means)**2 / (K - 1)
+within_vars = np.zeros(100)
+for v in vars_phns.itervalues():
+    within_vars += v / (N - K)
+F = between_vars / within_vars
+with open('F_phns_emb_' + suffix + '.npy', 'wb') as f:
+    F.dump(f)
